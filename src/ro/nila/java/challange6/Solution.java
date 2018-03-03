002eda3f681024ad944d4f2ec8a1f5c550cddba0
Alin Popa <popaalinradu@gmail.com>
2018-03-03 20:11:35 +0200

    solved java task 6

--- src/ro/nila/java/challange6/Solution.java
+++ src/ro/nila/java/challange6/Solution.java
@@ -1,5 +1,17 @@
 package ro.nila.java.challange6;
 
+import java.util.Scanner;
+
 public class Solution {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        for (int i = 1; i < 11; i++) {
+            int result;
+            result = N * i;
+            System.out.printf("%d x %d = %d%n", N, i, result);
+        }
+    }
 }
