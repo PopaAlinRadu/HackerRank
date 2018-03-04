94a6472e3caf97a7927e57c6fcf9da5a4108c288
Alin Popa <popaalinradu@gmail.com>
2018-03-04 10:01:39 +0200

    solved algorithms task 5

--- src/ro/nila/algorithms/challange5/Solution.java
+++ src/ro/nila/algorithms/challange5/Solution.java
@@ -1,4 +1,41 @@
 package ro.nila.algorithms.challange5;
 
+import java.util.Scanner;
+
 public class Solution {
+
+    static int diagonalDifference(int[][] a) {
+        int diagonalDifference = leftDiagonal(a) - rightDiagonal(a);
+        return Math.abs(diagonalDifference);
+    }
+
+    private static int leftDiagonal(int[][] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i][i];
+        }
+        return sum;
+    }
+
+    private static int rightDiagonal(int[][] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i][a.length - 1 - i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] a = new int[n][n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            for (int a_j = 0; a_j < n; a_j++) {
+                a[a_i][a_j] = in.nextInt();
+            }
+        }
+        int result = diagonalDifference(a);
+        System.out.println(result);
+        in.close();
+    }
 }
