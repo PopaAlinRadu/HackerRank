1da4b603bfd80135563da67d9a7683ac720b3ac6
Alin Popa <popaalinradu@gmail.com>
2018-03-05 09:36:35 +0200

    solved algorithms task 7

--- src/ro/nila/algorithms/challange7/Solution.java
+++ src/ro/nila/algorithms/challange7/Solution.java
@@ -1,4 +1,22 @@
 package ro.nila.algorithms.challange7;
 
+import java.util.Scanner;
+
 public class Solution {
+
+    static void staircase(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(j < n - i ? " " : "#");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        staircase(n);
+        in.close();
+    }
 }
